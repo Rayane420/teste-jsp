b176ff4ec660a827b8f3806290de48a95bad4484
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2020-08-12 08:59:57 -0300

    Add files via upload

---
+++ curso-jsp/src/beans/BeanCursoJsp.java
@@ -0,0 +1,162 @@
+package beans;
+
+public class BeanCursoJsp {
+
+	private Long id;
+
+	private String login;
+
+	private String senha;
+
+	private String nome;
+
+	private String telefone;
+
+	private String cep;
+
+	private String rua;
+
+	private String bairro;
+
+	private String cidade;
+
+	private String estado;
+
+	private String ibge;
+
+	private String fotoBase64;
+	private String curriculoBase64;
+
+	private String contentType;
+	private String contentTypeCurriculo;
+
+	private String tempFotoUser;
+
+	public String getTempFotoUser() {
+
+		tempFotoUser = "data:" + contentType + ";base64," + fotoBase64;
+
+		return tempFotoUser;
+	}
+
+	public String getCurriculoBase64() {
+		return curriculoBase64;
+	}
+
+	public void setCurriculoBase64(String curriculoBase64) {
+		this.curriculoBase64 = curriculoBase64;
+	}
+
+	public String getContentTypeCurriculo() {
+		return contentTypeCurriculo;
+	}
+
+	public void setContentTypeCurriculo(String contentTypeCurriculo) {
+		this.contentTypeCurriculo = contentTypeCurriculo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getFotoBase64() {
+		return fotoBase64;
+	}
+
+	public void setFotoBase64(String fotoBase64) {
+		this.fotoBase64 = fotoBase64;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+}
---
+++ curso-jsp/src/servlet/Usuario.java
@@ -0,0 +1,261 @@
+package servlet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import beans.BeanCursoJsp;
+import dao.DaoUsuario;
+
+/**
+ * Servlet implementation class Usuario
+ */
+@WebServlet("/salvarUsuario")
+@MultipartConfig
+public class Usuario extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private DaoUsuario daoUsuario = new DaoUsuario();
+
+	public Usuario() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			String acao = request.getParameter("acao");
+			String user = request.getParameter("user");
+
+			if (acao.equalsIgnoreCase("delete") && user != null) {
+				daoUsuario.delete(user);
+				RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp");
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+
+			} else if (acao.equalsIgnoreCase("editar")) {
+
+				BeanCursoJsp beanCursoJsp = daoUsuario.consultar(user);
+
+				RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp");
+				request.setAttribute("user", beanCursoJsp);
+				view.forward(request, response);
+
+			} else if (acao.equalsIgnoreCase("listartodos")) {
+				RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp"); // indicando para qual
+																								// tela vai redirecionar
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+			} else if (acao.equalsIgnoreCase("download")) {
+				BeanCursoJsp usuario = daoUsuario.consultar(user);
+				if (usuario != null) {
+
+					String contentType = "";
+					byte[] fileBytes = null;
+
+					String tipo = request.getParameter("tipo");
+
+					if (tipo.equalsIgnoreCase("imagem")) {
+						contentType = usuario.getContentType();
+						fileBytes = new Base64().decodeBase64(usuario.getFotoBase64());
+					} else if (tipo.equalsIgnoreCase("curriculo")) {
+						contentType = usuario.getContentTypeCurriculo();
+						fileBytes = new Base64().decodeBase64(usuario.getCurriculoBase64());
+					}
+
+					response.setHeader("Content-Disposition",
+							"attachment;filename=arquivo." + contentType.split("\\/")[1]);
+
+					/*
+					 * Recebendo os bytes convertidos acima em um objeto de entrada para processar
+					 */
+					InputStream is = new ByteArrayInputStream(fileBytes);
+
+					/* Iniciando resposta para o navegador */
+					int read = 0;
+					byte[] bytes = new byte[1024];
+					OutputStream os = response.getOutputStream();
+
+					while ((read = is.read(bytes)) != -1) {
+						os.write(bytes, 0, read);
+					}
+
+					os.flush();
+					os.close();
+
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+
+		if (acao != null && acao.equalsIgnoreCase("reset")) {
+			try {
+				RequestDispatcher view = request.getRequestDispatcher(
+						"/cadastroUsuario.jsp"); /* falando para permanecer na mesma página após o cadastro */
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response); // fazendo redirecionamento da página
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+
+			String id = request.getParameter("id");
+			String login = request.getParameter("login"); // recebendo o login que vem da requisição
+			String senha = request.getParameter("senha");
+			String nome = request.getParameter("nome");
+			String telefone = request.getParameter("telefone");
+			String cep = request.getParameter("cep");
+			String rua = request.getParameter("rua");
+			String bairro = request.getParameter("bairro");
+			String cidade = request.getParameter("cidade");
+			String estado = request.getParameter("estado");
+			String ibge = request.getParameter("ibge");
+
+			BeanCursoJsp usuario = new BeanCursoJsp();
+			usuario.setId((id != null && !id.isEmpty()) ? Long.parseLong(id) : null);
+			usuario.setLogin(login);
+			usuario.setSenha(senha);
+			usuario.setNome(nome);
+			usuario.setTelefone(telefone);
+			usuario.setCep(cep);
+			usuario.setRua(rua);
+			usuario.setBairro(bairro);
+			usuario.setCidade(cidade);
+			usuario.setEstado(estado);
+			usuario.setIbge(ibge);
+
+			try {
+
+				/* INICIO de File upload de imagens e pdf */
+				if (ServletFileUpload.isMultipartContent(request)) {
+					
+					Part imagemFoto = request.getPart("foto");
+					
+					if (imagemFoto != null && imagemFoto.getInputStream().available() > 0) {
+						String fotoBase64 = new Base64()
+								.encodeBase64String(converteStreamParabyte(imagemFoto.getInputStream()));
+
+						usuario.setFotoBase64(fotoBase64);
+						usuario.setContentType(imagemFoto.getContentType());
+					}else {
+						usuario.setFotoBase64(request.getParameter("fotoTemp"));
+						usuario.setContentType(request.getParameter("contentTypeTemp"));
+						
+					}
+					
+					/* processa pdf */
+					Part curriculoPdf = request.getPart("curriculo");
+					if (curriculoPdf != null && curriculoPdf.getInputStream().available() > 0) {
+						String curriculoBase64 = new Base64()
+								.encodeBase64String(converteStreamParabyte(curriculoPdf.getInputStream()));
+						usuario.setCurriculoBase64(curriculoBase64);
+						usuario.setContentTypeCurriculo(curriculoPdf.getContentType());
+
+					}
+
+				}
+				/* FINAL de File upload de imagens e pdf */
+
+				String msgL = null;
+				boolean podeInserir = true;
+
+				if (login == null || login.isEmpty()) {
+					msgL = "Login deve ser informado";
+					podeInserir = false;
+				} else if (senha == null || senha.isEmpty()) {
+					msgL = "Senha deve ser informado";
+					podeInserir = false;
+				} else if (nome == null || nome.isEmpty()) {
+					msgL = "Nome deve ser informado";
+					podeInserir = false;
+				} else if (telefone == null || telefone.isEmpty()) {
+					msgL = "Telefone deve ser informado";
+					podeInserir = false;
+				} else if (id == null || id.isEmpty() && !daoUsuario.validarLogin(login)) {
+					request.setAttribute("msg",
+							"Já existe um usuário com o login informado, favor informar login diferente!");
+					request.setAttribute("user", usuario);
+
+				} else if (id == null || id.isEmpty() && !daoUsuario.validarSenha(senha)) {
+					request.setAttribute("msg", "Essa senha já existe, favor cadastrar nova senha");
+					request.setAttribute("user", usuario); // mantendo dados carregados em tela
+					podeInserir = false;
+				}
+
+				if (msgL != null) {
+					request.setAttribute("msg", msgL);
+				} else if ((id == null || id.isEmpty()) && daoUsuario.validarLogin(login)
+						&& daoUsuario.validarSenha(senha) && podeInserir) {
+					daoUsuario.Salvar(usuario);
+					request.setAttribute("msgC", "Usuário cadastrado com sucesso!");
+
+				}
+
+				else if (id != null && !id.isEmpty() && podeInserir) {
+					if (!daoUsuario.validarLoginUpdate(login, id)) {
+						request.setAttribute("msg", "Este login já pertence a outro usuário");
+						request.setAttribute("user", usuario);
+					} else if (id != null && !id.isEmpty() && podeInserir) {
+						daoUsuario.atualizar(usuario);
+						request.setAttribute("msgC", "Usuário atualizado com sucesso!");
+					}
+				}
+				if (!podeInserir) {
+					request.setAttribute("user", usuario);
+				}
+
+				RequestDispatcher view = request.getRequestDispatcher(
+						"/cadastroUsuario.jsp"); /* falando para permanecer na mesma página após o cadastro */
+				request.setAttribute("usuarios", daoUsuario.listar());
+				request.setAttribute("msgC", "Salvo com sucesso");
+				view.forward(request, response); // fazendo redirecionamento da página
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	/* Converte a entrada de fluxo de dados da imagem para byte */
+	private byte[] converteStreamParabyte(InputStream imagem) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int reads = imagem.read();
+		while (reads != -1) {
+			baos.write(reads);
+			reads = imagem.read();
+		}
+		return baos.toByteArray();
+	}
+
+}
